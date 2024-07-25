3134e98041c0bb5dfa941d2f47406ee1afec0cd8
Noble Benjamin <nobleben2008@gmail.com>
2024-07-25 20:11:03 +0100

    Added a reducer function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
+import { useEffect, useReducer } from "react";
 
 function App() {
+  const initialState = {
+    questions: [],
+
+    status: "loading",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      default:
+        throw new Error('Action Unknown')
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // Fetch questions from API
+  useEffect(function () {
+    function fetch() {
+      fetch("http://localhost:3001/questions")
+        .then((response) => response.json())
+        .then((data) => dispatch({ type: "dataReceived", payload: data }));
+    }
+  }, []);
   return <Header />;
 }
 
