d32098fdbfa41e6c76f6c550240039e459b1e75c
Noble Benjamin <nobleben2008@gmail.com>
2024-07-25 18:50:41 +0100

    Made the header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <header className='app-header'>
-
+    <header className="app-header">
+      <img src="logo512.png" alt="React Logo" />
+      <h1>React Quiz</h1>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
