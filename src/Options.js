390394712996baf15318e9355e7be5d0feade45c
Noble Benjamin <nobleben2008@gmail.com>
2024-07-26 20:03:59 +0100

    Added a functionality to display a color when question is answered

--- src/Options.js
+++ src/Options.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Options({option, dispatch, index, answer, question}) {
   return (
-   <button className={`btn btn-option ${index === answer ? 'answer': ''}`} onClick={() => {
+   <button className={`btn btn-option ${index === answer ? 'answer': ''} ${answer !== null && (index === question.correctOption ? 'correct' : 'wrong')}`} onClick={() => {
         dispatch({type: 'answer', payload: index})
    }}>{option}</button>
   )
