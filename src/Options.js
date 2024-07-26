595b4e1de54fd9141035ebae955fab0f6b63c68c
Noble Benjamin <nobleben2008@gmail.com>
2024-07-26 20:05:09 +0100

    Added a functionality to prevent multiple answers

--- src/Options.js
+++ src/Options.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Options({option, dispatch, index, answer, question}) {
   return (
    <button className={`btn btn-option ${index === answer ? 'answer': ''} ${answer !== null && (index === question.correctOption ? 'correct' : 'wrong')}`} onClick={() => {
-        dispatch({type: 'answer', payload: index})
+    answer === null && dispatch({type: 'answer', payload: index})
    }}>{option}</button>
   )
 }
