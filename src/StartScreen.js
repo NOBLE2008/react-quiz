a5876d2e864082bbf449e19c1662961813b4af1b
Noble Benjamin <nobleben2008@gmail.com>
2024-08-02 12:44:00 +0100

    Refactored StartScreen.js to use Context

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useQuiz } from "../context/QuizContext";
 
-export default function StartScreen({questions, dispatch}) {
+export default function StartScreen() {
+  const { state, dispatch } = useQuiz();
+
+  const { questions } = state;
   return (
-    <div className='start'>
+    <div className="start">
       <h2>Welcome to React Quiz</h2>
       <h3>{questions.length} questions to test your React Mastery</h3>
-      <button className='btn' onClick={() => {
-        dispatch({type: 'start'})
-      }}>Let's Start</button>  
+      <button
+        className="btn"
+        onClick={() => {
+          dispatch({ type: "start" });
+        }}
+      >
+        Let's Start
+      </button>
     </div>
-  )
+  );
 }
